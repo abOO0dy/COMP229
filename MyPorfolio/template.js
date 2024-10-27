f182d986dff84d1517f4f2de3b23c12f8cd1bc93
Abdulrahman Hamid <abooym@gmail.com>
2024-10-27 18:56:12 -0400

    add server-side

---
+++ MyPorfolio/template.js
@@ -0,0 +1,13 @@
+export default () => { 
+    return `<!doctype html>
+    <html lang="en"> 
+    <head>
+    <meta charset="utf-8">
+    <title></title> 
+    </head>
+    <body>
+    <div id="root">Welcome to Abdulrahman's portfolio</div> 
+    </body>
+    </html>` 
+    }
+    
